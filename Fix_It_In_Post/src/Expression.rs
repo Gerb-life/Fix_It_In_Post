78579852e2496662863215679488ea2a597aa58c
josiah91287 <josiahcherbony@gmail.com>
2023-02-25 17:10:51 -0500

    Commit 4

--- Fix_It_In_Post/src/Expression.rs
+++ Fix_It_In_Post/src/Expression.rs
@@ -1,10 +1,8 @@
-
 use std::env;
-use std::error::Error;
 use std::fs::{File, OpenOptions};
-use std::io::{self , BufRead, BufReader, BufWriter, Write};
+use std::io::{BufRead, BufReader, BufWriter, Write, Error};
 use std::process::exit;
-use evalexpr::*; //Ask Kreahling if we can use this?
+
 
 
 ///The Expression "Object"
@@ -20,7 +18,7 @@ pub struct Expression{
 //Implementing the "Objects" methods 
 impl Expression {
     
-//constructor
+    //constructor
     fn new(input: String) -> Self{
         Expression { 
             postfix : input,
@@ -29,70 +27,70 @@ impl Expression {
          }
     }
 
-/**
- * Solves postfix expression and creates infix express
- * returns - none
- */
-
-fn solve(&mut self){
-    let mut stack = Vec::new();
-    let expressions = "*+/-";
+    /**
+    * Solves postfix expression and creates infix express
+    * returns - none
+    */
+    fn solve(&mut self){
+        let mut stack = Vec::new();
+        let expressions = "*+/-";
     
-    // Splitting over whitespaces gives us all the characters in our expression
-    for character in self.postfix.split_whitespace(){
-        if expressions.contains(character){
-            let val2 = stack.pop().unwrap();
-            let val1 = stack.pop().unwrap();
-            let mut result = 0.0;
+        // Splitting over whitespaces gives us all the characters in our expression
+        for character in self.postfix.split_whitespace(){
+            if expressions.contains(character){
+                let val2 = stack.pop().unwrap();
+                let val1 = stack.pop().unwrap();
+                let result;
 
-            match character{
-                "*" => result = val1 * val2,
-                "-" => result = val1 - val2,
-                "+" => result = val1 + val2,
-                "/" => result = val1 / val2,
-                &_ => todo!(), //For some reason< Rust is demanding we put this at the end
-            }
+                match character{
+                    "*" => result = val1 * val2,
+                    "-" => result = val1 - val2,
+                    "+" => result = val1 + val2,
+                    "/" => result = val1 / val2,
+                    &_ => todo!(), //For some reason< Rust is demanding we put this at the end
+                }
             
-            stack.push(result);
+                stack.push(result);
 
-        }else{//Else our character is a number 
-            //We convert the character to f64 and push it on the stack
-            let num = character.parse::<f64>().unwrap();
-            stack.push(num);
+            }else{//Else our character is a number 
+                //We convert the character to f64 and push it on the stack
+                let num = character.parse::<f64>().unwrap();
+                stack.push(num);
+            }
+
         }
-
-    }
-    self.expr = stack;
-
+        self.expr = stack;
     //print!("{:?}", stack);
-
-
-}
+    }
    
 
-fn postfix_to_infix(&mut self){
-    let mut stack:Vec<String> = Vec::new();
+    fn postfix_to_infix(&mut self){
+        let mut stack:Vec<String> = Vec::new();
     
 
-    for item in self.postfix.split_whitespace(){
-        //if operand push onto stack
-        if(!is_operator(item)){
-            stack.push(item.to_string());
+        for item in self.postfix.split_whitespace(){
+         //if operand push onto stack
+            if !is_operator(item){
+                stack.push(item.to_string());
+            }
+            //else pop 2 elements, combine and push onto stack
+            else{
+                let operand1 = stack.pop().unwrap();
+                let operand2 = stack.pop().unwrap();
+                let combine = format!("({} {} {})" ,operand2 , item , operand1 );
+                stack.push( combine ); 
+            } 
         }
-        //else pop 2 elements, combine and push onto stack
-        else{
-            let operand1 = stack.pop().unwrap();
-            let operand2 = stack.pop().unwrap();
-            let combine = format!("({} {} {})" ,operand2 , item , operand1 );
-            stack.push( combine ); 
-
-        }
-        
-        
+        //setting expression infix to be equal to clone of first element
+        self.infix = stack[0].clone();
     }
-    //setting expression infix to be equal to clone of first element
-    self.infix = stack[0].clone();
-}
+
+    fn to_string(& self) -> String{
+        // println!("Postfix:{} \nInfix: {} \nExpr: {:?}", expression.postfix , expression.infix, expression.expr);
+        let result = format!("{} = {:?}\n" , self.infix , self.expr);
+        return result;
+    }
+    
 
 }
 
@@ -123,17 +121,12 @@ fn postfix_to_infix(&mut self){
     
     solve_list(&mut expression_list);
     sort_list(&mut expression_list);
+    write_to_file("testOutput.txt", &mut expression_list).expect("Failed to create and write");
 
 
     for i in 0..expression_list.len(){
-        println!("{}" , to_string(&expression_list[i]));
+        print!("{}" , expression_list[i].to_string());
     }
-
-    
-   
-     
-    
-    
 
 }
 
@@ -141,8 +134,7 @@ fn postfix_to_infix(&mut self){
  * This function accepts a reference to a string slice representing the input file name
  * and returns a ‘Result’ with a vector of expressions from the file or an appropriate error.
  */
-
- fn build_expression_list(input_file: &str) -> Result<Vec<Expression> , io::Error>{
+fn build_expression_list(input_file: &str) -> Result<Vec<Expression> , Error>{
     let file = File::open(input_file);
     //checking not complete yet , works if file exists
     let check_file = match file{
@@ -167,75 +159,61 @@ fn postfix_to_infix(&mut self){
  * Takes a reference to a vector of Expressions and solves them. No return value.
  */
 fn solve_list(expression_list: &mut Vec<Expression>){
-    
     for i in 0..expression_list.len(){
         expression_list[i].solve();
         expression_list[i].postfix_to_infix();
-       }
-    
+       } 
 }
 
 /**
  * Takes a reference to a vector of Expressions and sorts them based on the value of the
  * expressions solution.
  */
-    fn sort_list(expression_list: &mut Vec<Expression>){
-        // We should pass the Vector to this method after we've pass it to solve_list()
-        let length = expression_list.len();
+fn sort_list(expression_list: &mut Vec<Expression>){
+    // We should pass the Vector to this method after we've pass it to solve_list()
+    let length = expression_list.len();
 
-        for i in 0..length{
-            
-            let smallest = find_smallest(expression_list, i);
-    
-            
-            expression_list.swap(smallest , i);
-        }
-
+    for i in 0..length{
+        let smallest = find_smallest(expression_list, i);   
+        expression_list.swap(smallest , i);
     }
-
-    fn find_smallest(expression_list: &mut Vec<Expression>, current: usize)-> usize{
-        let length = expression_list.len();
-        
-        let mut smallest = current;
-        
-        //if vector at i is less than smallest then change smallest
-        for i in (current +1)..length{
-            if expression_list[i].expr[0] < expression_list[smallest].expr[0]{
-                smallest = i;
-            }
-        }
-        
-        smallest
-    
-    }
-
-    /**
-    * This takes a reference to a string slice, representing the output file name and a reference
-    * to a vector of expressions. This function returns a ’Result’
-    */
-
-    fn write_to_file(output: &str , expression_list: &Vec<Expression>){
-
-    }
-
-
-
-
-
-
-
-
-fn to_string(expression: &Expression) -> String{
-    
-   // println!("Postfix:{} \nInfix: {} \nExpr: {:?}", expression.postfix , expression.infix, expression.expr);
-
-   let result = format!("{} = {:?}" , expression.infix , expression.expr);
-
-    
-
-
-    return result;
 }
+
+fn find_smallest(expression_list: &mut Vec<Expression>, current: usize)-> usize{
+    let length = expression_list.len(); 
+    let mut smallest = current;
+        
+    //if vector at i is less than smallest then change smallest
+    for i in (current +1)..length{
+        if expression_list[i].expr[0] < expression_list[smallest].expr[0]{
+            smallest = i;
+        }
+    }  
+    smallest
+}
+
+/**
+* This takes a reference to a string slice, representing the output file name and a reference
+ * to a vector of expressions. This function returns a ’Result’
+*/
+
+fn write_to_file(fileName: &str , expression_list: &Vec<Expression>) -> Result<(), Error> {
+    let mut created = File::create(fileName)?;
+
+    for expr in expression_list{
+        let output = expr.to_string();
+        created.write(output.as_bytes())?;
+    }
+    Ok(())
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -251,9 +229,5 @@ fn is_operator(character: &str) -> bool{
             result = true;
         }
     }
-    
-
-
     return result;
-
 }
