22e0b2d300245516d018a8744e792f64dabb217b
josiah91287 <josiahcherbony@gmail.com>
2023-03-03 18:17:50 -0500

    Commit 8

--- Fix_It_In_Post/src/Expression.rs
+++ Fix_It_In_Post/src/Expression.rs
@@ -1,9 +1,6 @@
 use std::env;
-use std::fs::{File, OpenOptions};
-use std::io::{BufRead, BufReader, BufWriter, Write, Error};
-use std::process::exit;
-
-
+use std::fs::{File};
+use std::io::{BufRead, BufReader, Write, Error as Err};
 
 /// The Expression "Object"
 /// 
@@ -16,13 +13,12 @@ pub struct Expression{
     infix: String,
 }
 
-
 //Implementing the "Objects" methods 
 impl Expression {
     
     /// Constructs a new 'Expression' object 
     /// 
-    /// * input - The input 
+    /// * 'input' - The postfix expression 
     fn new(input: String) -> Self{
         Expression { 
             postfix : input,
@@ -32,73 +28,51 @@ impl Expression {
     }
  
     ///
-    /// Solves postfix expression and creates infix express
+    /// Solves our postfix expression and creates infix expression.
+    /// Both are asssigned to our objects' fields.
     /// 
     fn solve(&mut self){
-        let mut stack = Vec::new();
-    
+        let mut stack:Vec<String> = Vec::new();
+
         // Splitting over whitespaces gives us all the characters in our expression
         for character in self.postfix.split_whitespace(){
-            if is_operator(character){ // If our character is a expression
-                    let val2 = stack.pop().unwrap();
-                    let val1 = stack.pop().unwrap();
-                    let result;
 
-                    match character{
-                        "*" => result = val1 * val2,
-                        "-" => result = val1 - val2,
-                        "+" => result = val1 + val2,
-                        "/" => result = val1 / val2,
-                        &_ => todo!(), // For some reason, Rust is demanding we put this at the end
-                    }
-            
-                    stack.push(result);
+            if is_operator(character){ // If our character is an operator 
+                // Getting the current 2 values in our stack 
+                let operand2 = stack.pop().unwrap();
+                let operand1 = stack.pop().unwrap();
+                let val2 = self.expr.pop().unwrap();
+                let val1 = self.expr.pop().unwrap();
+                let result;
+                
+                match character{
+                    "*" => result = val1 * val2,
+                    "-" => result = val1 - val2,
+                    "+" => result = val1 + val2,
+                    "/" => result = val1 / val2,
+                    &_ => todo!(), 
+                }
 
-            }else if is_digit(character){// Else our character is a number 
+                let combine = format!("({} {} {})" , operand1, character , operand2 );
+                stack.push( combine ); 
+                self.expr.push(result);
+
+            }else if is_num(character){// Else our character is a number 
                 // We convert the character to f64 and push it on the stack
                 let num = character.parse::<f64>().unwrap();
-                stack.push(num);
-            }
-        }
-        self.expr = stack;
-    }
-   
-    ///
-    ///  Converts 'postfix' to a infix expression.
-    /// 
-    fn postfix_to_infix(&mut self){
-        let mut stack:Vec<String> = Vec::new();
-        
-        // Gets all of the characters in a expression. 
-        for character in self.postfix.split_whitespace(){
-            if is_digit(character){ // If operand push onto stack
+                self.expr.push(num);
                 stack.push(character.to_string());
-                //println!("{:?}", stack);
             }
-            // Else pop 2 elements, combine and push onto stack
-            else if is_operator(character){
-                let operand1 = stack.pop().unwrap();
-                let operand2 = stack.pop().unwrap();
-                let combine = format!("({} {} {})" ,operand2 , character , operand1 );
-                stack.push( combine ); 
-            } 
         }
         if stack.len() == 1 { // If we end our loop with 1 value in the stack 
             // Make our expression's 'infix' field our infix expression 
             self.infix = stack[0].clone();
         }
     }
-    
-    ///
-    /// Formats our Epxression, shoing it in infix notation and its value.
+   
     /// 
-    fn to_string(& self) -> String{
-        let result = format!("{} = {:?}\n" , self.infix , self.expr[0]);
-        return result;
-    }
-
-
-    /// We need to RETURN AN ERROR MESSAGE if our expression is invalid 
+    /// A method that returns true if a postfix expression is valid, and false if it's  not. 
+    /// 
     fn is_valid(&self) -> bool{
         let mut valid = true;
         let mut stack:Vec<&str> = Vec::new();
@@ -113,16 +87,32 @@ impl Expression {
                 else { // Else this is an invalid expression
                     valid = false;
                 }
-            }else if is_digit(character){
+            }else if is_num(character){
                 stack.push(character)
             }
-            else{
+            else{ // Else if we come across an invalid character
                 valid = false;
             }
         }
         // The stack length should be 1, or this isn't a valid expression
         return valid && stack.len() == 1
     }   
+
+    ///
+    /// Formats our Epxression, shoing it in infix notation and its value.
+    /// 
+    fn to_string(& self) -> String{
+        let mssg;
+
+        // Removes trailing 0s from 'expr' that are integers 
+        if self.expr[0].floor() == self.expr[0]{
+            mssg = format!("{} = {:.0}\n" , self.infix, self.expr[0]);
+        }else{
+            mssg = format!("{} = {:?}\n" , self.infix, self.expr[0]);
+        }
+
+        return mssg;
+    }
     
 }
 
@@ -131,34 +121,25 @@ impl Expression {
 /// If there is an error writing to the output file, main should print an appropriate error message.
 /// 
  fn main(){
-
-    let mut _args: Vec<String> = env::args().collect();
+    // Getting our command line arguments 
+    let args: Vec<String> = env::args().collect();
     
-    //handles command line arguemnts commented out for testing
-    // if args.len() != 3 {
-    // println!("Usage: cargo run --example expr [INPUTFILE] [OUTPUTFILE]");
-    // return;  // acts as exit
-    //}
-
-    //Gets the 2nd index of the argument array (which should be the file we're pulling from)
-
-    
-    // setting input file to the first commane line argument 
-    //commented out for testing  
-    //let input_file: &str = &args[1];
-    let input_file = &_args[1];
-    //let output_file = &_args[2];
-    let mut expression_list: Vec<Expression> =  build_expression_list(input_file).unwrap();//if inside src directory path is input.txt
-    
-    solve_list(&mut expression_list);
-    sort_list(&mut expression_list);
-    write_to_file("testOutput.txt", &mut expression_list).expect("Failed to create and write");
-    
-    for i in 0..expression_list.len(){
-        println!("{}" , expression_list[i].to_string());
+    // We must have EXACTLY 3 arguments 
+    if args.len() != 3 {
+        println!("Usage: cargo run --example expr [INPUTFILE] [OUTPUTFILE]");
+        return;  // acts as exit
     }
-    
 
+    // Setting file names to command line arguments 
+    let in_file = &args[1];
+    let out_file = &args[2];
+    // Create a list of expressions 
+    let mut expression_list: Vec<Expression> =  build_expression_list(in_file)
+        .expect("Failed to build expression list!");
+    
+    solve_list(&mut expression_list); // Solves our list of expressions 
+    sort_list(&mut expression_list); // Sorts our expressions by output
+    write_to_file(out_file, &mut expression_list).expect("Failed to create file!");
 }
 
 
@@ -166,9 +147,9 @@ impl Expression {
 /// and returns a ‘Result’ with a vector of expressions from the file or an appropriate error.
 /// 
 /// * - 'file_name' The name of the file we're pulling expressions from. 
-fn build_expression_list(file_name: &String) -> Result<Vec<Expression> , Error>{
+fn build_expression_list(file_name: &String) -> Result<Vec<Expression> , Err>{
     let file = File::open(file_name);
-    //checking not complete yet , works if file exists
+    // Checking not complete yet , works if file exists
     let check_file = match file{
         Ok(file) => file,
         Err(e) => return Err(e)
@@ -180,11 +161,16 @@ fn build_expression_list(file_name: &String) -> Result<Vec<Expression> , Error>{
     //Gets all lines from file
     for lines in reader.lines(){
         let clone = lines.unwrap().clone();
-        if clone.len() > 0 {
+        if clone.len() > 0 { // The string must have more than 0 characters 
             let expression = Expression::new(clone);
-            if expression.is_valid() == true{
+            
+            if expression.is_valid(){ // If the expression is valid 
                 expression_list.push(expression);//Pushing a line from the file into expression list 
             }
+            else{ // Else the expression is invalid
+                println!("Error! '{:}' is not a valid expression!", expression.postfix);
+            }
+        
         }
     }
    
@@ -196,7 +182,6 @@ fn build_expression_list(file_name: &String) -> Result<Vec<Expression> , Error>{
 /// * 'expression_list' - A list of Expression objects
 fn solve_list(expression_list: &mut Vec<Expression>){
     for i in 0..expression_list.len(){
-        expression_list[i].postfix_to_infix();
         expression_list[i].solve();
     } 
 }
@@ -240,16 +225,15 @@ fn find_smallest(expression_list: &mut Vec<Expression>, current: usize)-> usize{
 /// 
 /// * - 'file_name' The name of the file we're pulling expressions from. 
 /// * 'expression_list' - A list of Expression objects.
-fn write_to_file(file_name: &str , expression_list: &Vec<Expression>) -> Result<(), Error> {
-    let mut created = File::create(file_name)?;
+fn write_to_file(file_name: &str , expression_list: &Vec<Expression>) -> Result<(), Err> {
+    let mut file = File::create(file_name)?;
 
     for expr in expression_list{
         let output = expr.to_string();
-        created.write(output.as_bytes())?;
+        file.write(output.as_bytes())?;
     }
     Ok(())
 }
-
 
 /// Returns true if the argument is one of the operators we're looking for.
 /// 
@@ -266,11 +250,11 @@ fn is_operator(character: &str) -> bool{
     return result;
 }
 
-/// Returns true if the argument is one of the characters we're looking for.
+/// Returns true if the argument is a number
 /// 
 /// * 'character' - The character we're checking. 
-fn is_digit (character: &str) -> bool{
-    let digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+fn is_num (character: &str) -> bool{
+    let digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
     let mut result = true;
 
     for element in character.chars(){
